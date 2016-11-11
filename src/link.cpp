ceef1a043b9c90f557f1c38e7ce4e349f056f7eb
ycgasjy <ycgasjy@gmail.com>
2016-11-11 16:36:15 -0500

    update

--- src/link.cpp
+++ src/link.cpp
@@ -4,10 +4,12 @@
 class link{
     private:
         int latency;
+        int direction;//0 is xpos, 1 is xneg, 2 is ypos, 3 is yneg, 4 is zpos, 5 is zneg
         node* src_node;
         node* dst_node;
         phit* phit_list;
-        bool back_pressure;//ture: turn on the backpressure to sender. false: no backpressure to sender
+        bool back_pressure;//ture: turn on the backpressure to sender. false: no backpressure to sender 
+                           //default: no backpressure
     public:
         //default constructor
         link();
@@ -15,11 +17,14 @@ class link{
         void link_alloc();
         void set_src();
         void set_dst();
+        void set_dir();
+        void advance_one_cycle()
 
 };
 
 link::link(){
     latency=28;
+    back_pressure=false;
 }
 
 void link::set_src(node* src){
@@ -31,18 +36,29 @@ void link::set_dst(node* dst){
     dst_node=dst;
 }
 
-
+void link::set_dir(int dir){
+    direction=dir;
+}
 
 void link::link_alloc(){
     phit_list=new phit[latency];
+
+    //phit[0] conncect to sender
+    //phit[latency-1] connect to receiver
     for(int i=0; i<latency; ++i){
         phit_list[i].valid=false;
         phit_list[i].cur_link=this;
     }
 }
 
-void link::advance_one_clock(){
-    for(int i=0;i<la
+void link::advance_one_cycle(){
+    for(int i=0;i<latency;++i){
+        if(back_pressure){
+            
+        }
+        phit_list[0]=src_node
+        
+    }
 
 }
 
---
+++ src/settings.h
@@ -0,0 +1,15 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#define PHIT_SIZE 8 //in bytes
+
+#define XPOS 1
+#define XNEG 2
+#define YPOS 3
+#define YNEG 4
+#define ZPOS 5
+#define ZNEG 6
+
+
+
+#endif
